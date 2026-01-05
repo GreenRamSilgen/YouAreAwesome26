dfb59d7b4cabf169edd3db1c8bc0cce4869260b6
GreenRamSilgen <kiranshrs@yahoo.com>
2026-01-05 07:47:18 -0800

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Kiran Shrestha on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
