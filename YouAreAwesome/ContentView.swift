c7623499d19009cc1e66f3362aa1522f8dfbb81a
GreenRamSilgen <kiranshrs@yahoo.com>
2026-01-06 08:39:59 -0800

    Ch 1.3 complete.

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -12,19 +12,30 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            Spacer()
+            
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
+                .fontWeight(.ultraLight)
             
-            Button("Click Me!") {
-                message = "Awesome"
-                print(message)
+            Spacer()
+            
+            HStack{
+                Button("Awesome!") {
+                    message = "Awesome!"
+                }
+                Button("Great!") {
+                    message = "Great!"
+                }
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
     }
