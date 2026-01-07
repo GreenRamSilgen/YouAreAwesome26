b1d035dafb162cb7527b9107367bccb77e92f42f
GreenRamSilgen <kiranshrs@yahoo.com>
2026-01-07 10:55:09 -0800

    Ch 1.9, 1.10, 1.11, 1.12

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,42 +9,69 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = ""
-    @State private var sysImg = ""
+    @State private var imageName = ""
+    @State private var lastMessageNumber : Int = -1
+    @State private var lastImageNumber : Int = -1
     
     var body: some View {
         VStack {
-            Spacer()
-            
-            Image(sysImg)
-                .resizable()
-                .scaledToFit()
-                .clipShape(RoundedRectangle(cornerRadius: 30))
-                .shadow(radius: 30)
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .frame(height: 100)
+                .minimumScaleFactor(0.5)
+                .animation(.easeInOut(duration: 0.2), value: message)
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+                .animation(.default, value: imageName)
             
             Spacer()
             
             HStack{
-                let image0 = "image0"
-                let message0 = "You Are Awesome!"
-                let image1 = "image1"
-                let message1 = "You Are Greaet!"
-                let image2 = "image2"
-                let image3 = "image3"
-                let image4 = "image4"
-                let image5 = "image5"
-                let image6 = "image6"
-                let image7 = "image7"
-                let image8 = "image8"
-                let image9 = "image9"
-                
+                let message0 = "If a woodchuck could chuck wood. Would a woodchuck chuck wood?"
+                let message1 = "You Are Great!"
+                let message2 = "You Are Alright!"
+                let message3 = "You Are Noice!"
+                let message4 = "You Are Cool!"
+                let message5 = "You Are Fantastic!"
+                let message6 = "You Are Super!"
+                let message7 = "You Are Amazing!"
+                let message8 = "You Are Dazzaling!"
+                let message9 = "You Are Wow!"
+                let messages = [
+                    message0,
+                    message1,
+                    message2,
+                    message3,
+                    message4,
+                    message5,
+                    message6,
+                    message7,
+                    message8,
+                    message9
+                ]
                 Button("Press Me!") {
-                    message = message == message0 ? message1 : message0
-                    sysImg = sysImg == image1 ? image0 : image1
+                    //TODO: - Update the imageName variable -
+                    var messageNumber : Int
+                    repeat {
+                        messageNumber = Int.random(in: 0..<messages.count)
+                    } while lastMessageNumber == messageNumber
                     
+                    message = messages[messageNumber]
+                    lastMessageNumber = messageNumber
+                    
+                    
+                    var imageNumber : Int
+                    repeat {
+                        imageNumber = Int.random(in: 0...9)
+                    } while lastImageNumber == imageNumber
+                    imageName = "image\(imageNumber)"
+                    lastImageNumber = imageNumber
                 }
             }
             .buttonStyle(.borderedProminent)
